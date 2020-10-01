f9546d8e7aa715f342bf8046a0c78e425961515c
jewkoiyie <58336567+jewkoiyie@users.noreply.github.com>
2020-10-01 23:56:58 +0530

    Update list.py
    it will print range of number which is taken by user from initial range to final range

--- list.py
+++ list.py
@@ -1,2 +1,10 @@
-list(range(3,6))
-print(list)
+print("This program  print make a list for a range that is take input from user ")
+l=int(input("L : ")) #initial range
+e=int(input("E : ")) #Final range
+li=[]
+try:
+   for j in range(l,e+1):
+       li.append(j)
+except:
+       print("please Put value sequence wise as L<E")
+print(li)
